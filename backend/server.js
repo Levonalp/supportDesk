50157907b786150c25c25588d44d2678623c3a9a
levonal <lwshabee@gmail.com>
2023-04-07 14:09:42 -0500

    make routes and controllers, test through postman

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("server....");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "welcome to the support desk API" });
+});
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(port, () => console.log(`server listening on port ${port}`));
