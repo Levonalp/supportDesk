744a1921a9919bb9add1bf76cd1eebf3bac72644
levonal <lwshabee@gmail.com>
2023-04-07 20:15:55 -0500

    make routes and controllers, test through postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,26 @@
-const registerUser = (req, res) => {
-  res.send("Register Route");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+// @desc Register a  new user
+// @route /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("please include all fields");
+  }
+  res.send("Register Route");
+});
+
+// @desc login a  new user
+// @route /api/users
+//@access Public
+
+const loginUser = asyncHandler(async (req, res) => {
   res.send("login Route");
-};
+});
 module.exports = {
   registerUser,
   loginUser,
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 8000;
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.status(200).json({ message: "welcome to the support desk API" });
 });
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server listening on port ${port}`));
