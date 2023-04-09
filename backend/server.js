106a37655529bba19f858f08ef5daebd52a3d026
levonal <lwshabee@gmail.com>
2023-04-08 22:04:48 -0500

    connect to database/config

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 8000;
 const app = express();
+
+// connect to db
+connectDB();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
