6aa2b3db2c1d79d30e5867466c1120e6993129d8
levonal <lwshabee@gmail.com>
2023-04-09 14:21:35 -0500

    add and test user model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,28 +1,57 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
 
-// @desc Register a  new user
-// @route /api/users
-//@access Public
+const User = require("../models/userModel");
+
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  //Validation
+  // Validation
   if (!name || !email || !password) {
     res.status(400);
-    throw new Error("please include all fields");
+    throw new Error("Please include all fields");
   }
-  res.send("Register Route");
+
+  // Find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new error("Invalid user data");
+  }
 });
 
-// @desc login a  new user
-// @route /api/users
-//@access Public
-
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send("login Route");
+  res.send("Login Route");
 });
-
-//orgnize login a new user
 
 module.exports = {
   registerUser,
