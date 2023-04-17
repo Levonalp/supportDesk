e5c262541cc9ae3de0925b79d71a5f6470c7056c
levonal <lwshabee@gmail.com>
2023-04-16 19:12:22 -0500

    prepare for deploy

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_N_URL || "/api/notes/";
+const API_URL = process.env.REACT_APP_T_URL || "/api/tickets/";
 
 // Get ticket notes
 const getNotes = async (ticketId, token) => {
